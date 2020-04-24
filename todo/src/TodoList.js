3c72e4b9575c87395ec2eb45aabbce62e063c9e6
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-04-24 16:39:26 -0700

    fixed reducer a bit, stuck with render

--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -1,19 +1,43 @@
-import React, { useReducer } from "react";
+import React, { useState, useReducer } from "react";
 import { initialState, reducer } from "./reducers/reducers";
 
 const TodoList = () => {
+  const [newTodoText, setNewTodoText] = useState("");
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const handleChanges = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state.task);
+  };
+
   return (
-    <div>
-        <h1>Todo</h1>
-      <div>
-        <input type="text"></input>
-        <button onClick={() => }>Add</button>
+    <div className="wrapper">
+      <h1>Todo</h1>
+      <div className="add-form">
+        <input
+          className="add-todo"
+          type="text"
+          name="newTodoText"
+          value={newTodoText}
+          onChange={handleChanges}
+        />
+        >
+        <button
+          onClick={() => {
+            dispatch({ type: "ADD_TODO", payload: newTodoText });
+            setNewTodoText("");
+          }}
+        >
+          Add
+        </button>
       </div>
-      <div>
-        <p>{}</p>
-        <button></button>
+      <div className="list">
+        <ul>
+          <li>{state.todos}</li>
+        </ul>
+        <button onClick={() => dispatch({ type: "CLEAR_COMPLETED" })}>
+          Clear Completed
+        </button>
       </div>
     </div>
   );
--- todo/src/reducers/reducers.js
+++ todo/src/reducers/reducers.js
@@ -1,16 +1,48 @@
-import React, { useReducer } from "react";
-
 const initialState = {
-  item: "Learn about reducers",
-  completed: false,
-  id: 1,
+  todos: [
+    {
+      item: "Learn about reducers",
+      completed: false,
+      id: 1,
+    },
+  ],
 };
 
-function reducer(state, action) {
+const reducer = (state, action) => {
   switch (action.type) {
+    case "ADD_TODO":
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            item: action.payload,
+            completed: false,
+            id: Date.now(),
+          },
+        ],
+      };
+
+    case "TOGGLE_COMPLETED":
+      return {
+        ...state,
+        todos: state.todos.map((item) => {
+          if (item.id === action.payload) {
+            return { ...item, done: !item.completed };
+          } else {
+            return item;
+          }
+        }),
+      };
+
+    case "CLEAR_COMPLETED":
+      return {
+        ...state,
+        todos: state.todos.filter((item) => !item.completed),
+      };
     default:
       return state;
   }
-}
+};
 
 export { initialState, reducer };
