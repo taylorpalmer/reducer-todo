ed8ff45dfa2a004bc8949e6764e8942618d2479f
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-05-06 14:11:46 -0700

    stopped h1 from rendering more than once

--- todo/src/App.js
+++ todo/src/App.js
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Todo</h1>
       <TodoList dispatch={dispatch} state={state} />
       <TodoForm dispatch={dispatch} />
     </div>
--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -4,7 +4,6 @@ import "../App.css";
 const Todo = (props) => {
   return (
     <div className="wrapper">
-      <h1>Todo</h1>
       <div
         className={`todo ${props.todo.completed ? "completed" : ""}`}
         onClick={() => props.toggleComplete(props.todo.id)}
